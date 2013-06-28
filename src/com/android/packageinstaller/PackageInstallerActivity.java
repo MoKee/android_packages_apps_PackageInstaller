71a6da7ea717a1ff8388aa5a4599beed39ffdbe1
martincz <martincz@mokeetek.com>
2013-06-28 22:29:07 +0800

    PackageInstaller: update
    Change-Id: Ie90a33f4e5a86f650cdec550bcc62da6e94ba8e3

--- src/com/android/packageinstaller/PackageInstallerActivity.java
+++ src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -309,7 +309,9 @@ public class PackageInstallerActivity extends Activity implements OnCancelListen
         mOk = (Button)findViewById(R.id.ok_button);
         mCancel = (Button)findViewById(R.id.cancel_button);
         mLocation = (Spinner)findViewById(R.id.install_location);
-        mLocation.setVisibility(Environment.isExternalStorageRemovable() ? Spinner.VISIBLE : Spinner.GONE);
+        File MMC = new File("/storage/sdcard0");
+        File EXTMMC = new File("/storage/sdcard1");
+        mLocation.setVisibility(MMC.exists() && EXTMMC.exists() || Environment.isExternalStorageRemovable() ? Spinner.VISIBLE : Spinner.GONE);
         mOk.setOnClickListener(this);
         mCancel.setOnClickListener(this);
         if (mScrollView == null) {
