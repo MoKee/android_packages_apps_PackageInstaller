662dacff519c4dbead8d3ba488aa053e865f3216
martincz <martincz@mokeedev.com>
2016-03-17 12:27:41 +0800

    PackageInstaller: Allow this installation only
    Change-Id: I34aa8d5f3af6780d7b75f8d028a219f75bbe8412

--- src/com/android/packageinstaller/PackageInstallerActivity.java
+++ src/com/android/packageinstaller/PackageInstallerActivity.java
@@ -257,6 +257,10 @@ public class PackageInstallerActivity extends Activity implements OnCancelListen
             return new AlertDialog.Builder(this)
                     .setTitle(R.string.unknown_apps_dlg_title)
                     .setMessage(R.string.unknown_apps_dlg_text)
+                    .setNeutralButton(R.string.just_once, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            initiateInstall();
+                        }})
                     .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Log.i(TAG, "Finishing off activity so that user can navigate to settings manually");
